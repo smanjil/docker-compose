61eef7142150ee9cde4cbadcff7bf469094abd0d
Peerity <jan@peerity.io>
2018-08-25 18:26:04 +0200

    host fixed

--- real-world-vue/src/components/Form.vue
+++ real-world-vue/src/components/Form.vue
@@ -73,7 +73,7 @@
             formData.append('file', this.fpath);
             console.log(formData);
 
-            this.axios.post('http://127.0.0.1:5000/api/post',
+            this.axios.post('http://34.215.86.75:5000/api/post',
               formData,
               {
                 headers: {
